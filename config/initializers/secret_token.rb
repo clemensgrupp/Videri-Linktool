636e031ad2fa5edfe6293d3fb7194fe988ddfb01
Jakob Beyer <jakob@videri-concept.de>
2011-12-05 11:35:53 +0100

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linktool::Application.config.secret_token = 'eefab25957f8276a87aefb6ed41ebcaaccc8d616decfcf28ba1b70c2c4c5c2e10fbe5c15ed02bc20aebcfe4416159624595c078a233006456a8ce2ea5ddd8d83'
